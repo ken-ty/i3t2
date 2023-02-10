1be20d1d67c56b995aaf27670a7b7d38d8493789
todoggler <kentokuraCS@gmail.com>
2023-02-11 04:06:57 +0900

    style: lint

--- lib/src/core/routes.dart
+++ lib/src/core/routes.dart
@@ -35,48 +35,54 @@ class Routes {
   /// ページテーブル
   static final pages = <PageInfo>[
     PageInfo(
-        name: 'ホーム',
-        description: """
+      name: 'ホーム',
+      description: """
 ホームです.
 初期のカウンター機能のままです.""",
-        routeName: '/home',
-        routing: (context) => const MyHomePage(title: 'ホーム')),
+      routeName: '/home',
+      routing: (context) => const MyHomePage(title: 'ホーム'),
+    ),
     PageInfo(
-        name: 'ページ一覧',
-        description: """
+      name: 'ページ一覧',
+      description: """
 全てのページを表示します.
 リストアイテムをタップするとページ詳細に遷移します.
 設計資料として利用できるページ一覧をCSV出力したいです(工事中)""",
-        routeName: '/dev/pages',
-        routing: (context) => const PagesPage()),
+      routeName: '/dev/pages',
+      routing: (context) => const PagesPage(),
+    ),
     PageInfo(
-        name: '空ページ',
-        description: """
+      name: '空ページ',
+      description: """
 何も実装しない空のページです.
 なにもいらないけど空画面が欲しい時に利用できます.""",
-        routeName: '/dev/empty',
-        routing: (context) => const EmptyPage(title: '空ページ')),
+      routeName: '/dev/empty',
+      routing: (context) => const EmptyPage(title: '空ページ'),
+    ),
     PageInfo(
-        name: '白紙1ページ',
-        description: """
+      name: '白紙1ページ',
+      description: """
 白紙ページです.
 これから実装する画面として一旦配置したりできます.""",
-        routeName: '/dev/blank1',
-        routing: (context) => const Blank1Page(title: '白紙1ページ')),
+      routeName: '/dev/blank1',
+      routing: (context) => const Blank1Page(title: '白紙1ページ'),
+    ),
     PageInfo(
-        name: '白紙2ページ',
-        description: """
+      name: '白紙2ページ',
+      description: """
 白紙ページです.
 これから実装する画面として一旦配置したりできます.""",
-        routeName: '/dev/blank2',
-        routing: (context) => const Blank2Page(title: '白紙2ページ')),
+      routeName: '/dev/blank2',
+      routing: (context) => const Blank2Page(title: '白紙2ページ'),
+    ),
     PageInfo(
-        name: '白紙3ページ',
-        description: """
+      name: '白紙3ページ',
+      description: """
 白紙ページです.
 これから実装する画面として一旦配置したりできます.""",
-        routeName: '/dev/blank4',
-        routing: (context) => const Blank3Page(title: '白紙3ページ')),
+      routeName: '/dev/blank4',
+      routing: (context) => const Blank3Page(title: '白紙3ページ'),
+    ),
   ];
 
   /// 初期ページのルート名
