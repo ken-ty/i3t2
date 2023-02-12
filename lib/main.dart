15ad699362832950b621269ce76885d4c8e96695
todoggler <kentokuraCS@gmail.com>
2023-02-12 22:12:49 +0900

    feat: env 読み取れるようにした

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:i3t2/config/app.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: "assets/env/.env");
   runApp(const App());
 }
